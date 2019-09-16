647c230386b1e53ae403741e52789871e37b67c2
VBPFelipe <felipe.vbpinheiro@gmail.com>
2019-09-16 18:03:26 -0300

    adds a commentary explaining code.

--- challenge 01 - monster killer/app.js
+++ challenge 01 - monster killer/app.js
@@ -13,23 +13,24 @@ new Vue({
         }
     },
     methods: {
-        start_game(){
+        start_game() {
             this.running = true
             this.player_life = 100
             this.monster_life = 100
             this.logs = []
         },
-        attack(special){
+        attack(special) {
             this.hurt('monster_life', 5, 10, special, 
                 'Jogador', 'Monstro', 'player')
             if(this.monster_life > 0)
                 this.hurt('player_life', 7, 12, false, 
                     'Monstro', 'Jogador', 'monster')
         },
-        hurt(prop, min, max, special, source, target, cls){
+        hurt(atr, min, max, special, source, target, cls) {
             const plus = special ? 5 : 0
             const hurt = this.get_random(min + plus, max + plus)
-            this[prop] = Math.max(this[prop] - hurt, 0)
+            //this[prop] - acessa em this o nome da variável
+            this[atr] = Math.max(this[prop] - hurt, 0)
             this.register_log(
                 `${source} atingiu ${target} com ${hurt}.`, cls
             )
@@ -40,23 +41,25 @@ new Vue({
                 'Monstro', 'Jogador', 'monster'
             )
         },
-        heal(min, max){
+        heal(min, max) {
             const heal = this.get_random(min, max)
             this.player_life = Math.min(this.player_life + heal, 100)
             this.register_log(
                 `Jogador ganhou força de ${heal}.`, 'player'
             )
         },
-        get_random(min, max){
+        get_random(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
         },
-        register_log(text, cls){
+        register_log(text, cls) {
             this.logs.unshift({ text, cls })
         }
     },
+    //Monitora  eventos, variáveis, métodos...
     watch: {
-        has_result(value){
+        // monitora has_result(value)
+        has_result(value) {
             if (value) this.running = false
         }
     }
