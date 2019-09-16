2650c3d8787c9602b04904c90169fd2cf7a75db1
VBPFelipe <felipe.vbpinheiro@gmail.com>
2019-09-16 17:21:26 -0300

    inserts gameplay functionalities, attacks, heals, damage generator and message logs

--- challenge 01 - monster killer/app.js
+++ challenge 01 - monster killer/app.js
@@ -1,18 +1,63 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 0,
-        monsterLife: 10
+        running: false,
+        player_life: 100,
+        monster_life: 100,
+        logs: []
     }, 
+    // funções pré-definidas na aplicação
     computed: {
-        hasResult() {
-            return this.playerLife == 0 || this.monsterLife == 0
+        has_result() {
+            return this.player_life == 0 || this.monster_life == 0
         }
     },
     methods: {
-
+        start_game(){
+            this.running = true
+            this.player_life = 100
+            this.monster_life = 100
+            this.logs = []
+        },
+        attack(special){
+            this.hurt('monster_life', 5, 10, special, 
+                'Jogador', 'Monstro', 'player')
+            if(this.monster_life > 0)
+                this.hurt('player_life', 7, 12, false, 
+                    'Monstro', 'Jogador', 'monster')
+        },
+        hurt(prop, min, max, special, source, target, cls){
+            const plus = special ? 5 : 0
+            const hurt = this.get_random(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.register_log(
+                `${source} atingiu ${target} com ${hurt}.`, cls
+            )
+        },
+        heal_and_hurt() {
+            this.heal(10, 15)
+            this.hurt('player_life', 7, 12, false, 
+                'Monstro', 'Jogador', 'monster'
+            )
+        },
+        heal(min, max){
+            const heal = this.get_random(min, max)
+            this.player_life = Math.min(this.player_life + heal, 100)
+            this.register_log(
+                `Jogador ganhou força de ${heal}.`, 'player'
+            )
+        },
+        get_random(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        register_log(text, cls){
+            this.logs.unshift({ text, cls })
+        }
     },
     watch: {
-        
+        has_result(value){
+            if (value) this.running = false
+        }
     }
 })
