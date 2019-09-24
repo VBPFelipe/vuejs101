33ddb9523e95ff355b77b4f7f45ac149ad02d1a7
VBPFelipe <felipe.vbpinheiro@gmail.com>
2019-09-23 21:02:09 -0300

    adds communication between components VueJs tutorial chapter

---
+++ vue-cli/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{ nome }}</strong></p>
+        <p>Idade é <strong>{{ idade }}</strong></p>
+        <button @click="alterarNome()">Alterar Nome</button> 
+        <hr>
+        <div class="componentes">
+            <app-usuario-info v-bind:nome="nome"
+                :idade="idade"
+                @nomeMudou="nome=$event.old+$event.novo" 
+                :reiniciarFn="reiniciarNome"/>
+            <!-- <app-usuario-info :nome="nome"/> -->
+            <app-usuario-editar :idade="idade" 
+                @idadeMudou="idade = $event + 1"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import UsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo: UsuarioInfo, AppUsuarioEditar },
+    data(){
+        return{
+            nome: 'Pedro',
+            idade: 21
+        }
+    },
+    methods: {
+        alterarNome(){
+            this.nome = 'Ana'
+        },
+        reiniciarNome(){
+            this.nome = 'Pedro'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ vue-cli/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong> {{ inverterNome() }} </strong></p>
+        <p>Idade do Usuário <strong>{{idade}}</strong></p>
+        <button @click="reiniciarNome"> Reiniciar Nome</button>
+        <button @click="reiniciarFn()"> Reiniciar Nome (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    //Estabelece o tipo da variável.
+    //Facilitador para validação
+    props:{
+        //Objeto
+        nome: {
+            type: String,
+            // type: [String, Array]
+            required: true, //propriedade é obrigatória para a funcionalidade do componente
+            default : 'Anônimo'
+            // default: function(){
+            //     return Array(10).fill(0).join(',')
+            // }
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods: {
+        inverterNome(){
+            return this.nome.split('').reverse().join('');
+        },
+        reiniciarNome(){
+            const old = this.nome
+            this.nome = "Pedro"
+            //dispara um evento para acessos externos
+            // this.$emit('nomeMudou', this.nome)
+            this.$emit('nomeMudou', {
+                novo: this.nome,
+                old
+            })
+        },
+        reiniciarFn(){
+
+        }
+    }
+}
+</script>
+
+// apenas neste escopo
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
